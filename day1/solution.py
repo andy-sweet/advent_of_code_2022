ea13614a524a602ecc4a14b95e44c9503782579e
Andy Sweet <andrew.d.sweet@gmail.com>
2022-12-01 19:40:00 -0800

    Add and solve part 2

--- day1/solution.py
+++ day1/solution.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 from typing import List
-
-def max_elf_calories(elf_item_calories: List[List[int]]) -> int:
-    return max(sum(item) for item in elf_item_calories)
+from heapq import heappop, heappush
 
 def read_elf_item_calories(path: Path) -> List[List[int]]:
     elf_item_calories = [[]]
@@ -14,6 +12,12 @@ def read_elf_item_calories(path: Path) -> List[List[int]]:
             else:
                 elf_item_calories[-1].append(int(item))
     return elf_item_calories
+
+def max_elf_calories(elf_item_calories: List[List[int]]) -> int:
+    return max(sum(item) for item in elf_item_calories)
+
+def top_three_elf_calories(elf_item_calories: List[List[int]]) -> int:
+    return sum(sorted(sum(item) for item in elf_item_calories)[-3:])
 
 TEST_ELF_ITEM_CALORIES = [
     [1000, 2000, 3000],
@@ -32,10 +36,15 @@ def test_max_elf_calories():
     actual = max_elf_calories(TEST_ELF_ITEM_CALORIES)
     assert actual == 24000
 
+def test_top_three_calories():
+    actual = top_three_elf_calories(TEST_ELF_ITEM_CALORIES)
+    assert actual == 45000
+
 def main():
     path = Path('input.txt')
     elf_item_calories = read_elf_item_calories(path)
-    print(max_elf_calories(elf_item_calories))
+    print(f'part 1: {max_elf_calories(elf_item_calories)}')
+    print(f'part 2: {top_three_elf_calories(elf_item_calories)}')
 
 if __name__ == '__main__':
     main()
